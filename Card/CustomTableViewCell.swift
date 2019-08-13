c9e8f854becb1b6d8b38fb8b772ca703e6f906c2
shotaro427 <toshinobu.s111@gmail.com>
2019-08-13 10:29:59 +0900

    cellのxibファイルを作成

---
+++ Card/CustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell.swift
+//  Card
+//
+//  Created by 田内　翔太郎 on 2019/08/13.
+//  Copyright © 2019 原田悠嗣. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
